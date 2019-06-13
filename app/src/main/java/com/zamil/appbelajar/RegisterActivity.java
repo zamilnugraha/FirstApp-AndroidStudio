eb565b061f1ab622685d06aeaf55411ac06a3c6e
zamilnugraha <zamil.nugraha@gmail.com>
2019-06-13 19:30:10 +0700

    Initial commit

---
+++ app/src/main/java/com/zamil/appbelajar/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.zamil.appbelajar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        setTitle("Register");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
